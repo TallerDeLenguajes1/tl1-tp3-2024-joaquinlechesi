409ad40832f3116b0b2b3f7049d9393cd81ab09b
joaquinlechesi <joaquinlechesi@gmail.com>
2024-04-26 13:01:46 -0300

    tp3_5

---
+++ tp3_5.c
@@ -0,0 +1,54 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+//#define cantidad 5 //cambio la cantidad
+
+void cargarNombres(char **lista, int cantidad);
+void mostrarNombres(char **lista, int cantidad);
+
+int main()
+{
+    char **lista; //*buff, lista es parte de la funcion main
+    //buff = (char *)malloc(50 * sizeof(char)); //bien
+    int cantidad;
+    puts("Ingrese la cantidad de nombres:");
+    scanf("%d", &cantidad);
+    lista = (char **)malloc(sizeof(char *) * cantidad); //tener en cuenta como se castea un puntero doble //bien
+    //char nombres[cantidad], *p_nombres;
+    cargarNombres(lista, cantidad);
+    mostrarNombres(lista, cantidad);
+    free(lista); //libero el puntero
+
+    return 0;
+}
+
+void cargarNombres(char **lista, int cantidad){
+    char *buff;
+    buff = (char *)malloc(50 * sizeof(char));
+    int tamanio;
+    for (int i = 0; i < cantidad; i++)
+    {
+        //puts("Ingrese un nombre:");
+        printf("Ingrese el nombre numero %d:\n", i+1);
+        fflush(stdin);
+        //scanf("%s", buff);
+        gets(buff);
+        tamanio = strlen(buff); //longitud de la cadena sin "/0"
+        //char *cadena;
+        //cadena = (char *)malloc(tamanio * sizeof(char)+1);
+        //strcpy(cadena, buff);
+        //lista = &cadena;
+        //printf("\n%p", lista);
+        //lista++;
+        lista[i] = (char *)malloc(sizeof(char) * tamanio+1);
+        strcpy(lista[i], buff);
+    }
+    free(buff);
+}
+
+void mostrarNombres(char ** lista, int cantidad){
+    for (int i = 0; i < cantidad; i++)
+    {
+        printf("\nEl nombre numero %d es %s", i+1, lista[i]);
+    }
+}
